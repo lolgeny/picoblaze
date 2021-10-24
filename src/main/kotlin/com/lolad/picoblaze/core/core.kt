95a30d97b03ca67bfa37034e1b2ad55c3448955d
lolgeny <joel@swedensky.com>
2021-10-24 21:51:03 +0100

    Added structure

--- src/main/kotlin/com/lolad/picoblaze/core/core.kt
+++ src/main/kotlin/com/lolad/picoblaze/core/core.kt
@@ -1,5 +1,6 @@
 package com.lolad.picoblaze.core
 
+import com.lolad.picoblaze.PicoBlaze
 import com.mojang.brigadier.CommandDispatcher
 import net.minecraft.client.MinecraftClient
 import net.minecraft.server.command.ServerCommandSource
@@ -7,15 +8,18 @@ import net.minecraft.server.command.ServerCommandSource
 object Events {
     val command: Hook<CommandDispatcher<ServerCommandSource>> = Hook()
     val client: Hook<MinecraftClient> = Hook()
+    val init: Hook<PicoBlaze> = Hook()
 }
 
 open class Module {
     init {
         Events.command.subscribe(::command)
         Events.client.subscribe(::client)
+        Events.init.subscribe(::init)
     }
     open fun command(dispatcher: CommandDispatcher<ServerCommandSource>) {}
     open fun client(client: MinecraftClient) {}
+    open fun init(mod: PicoBlaze) {}
 }
 
 class Hook<A> {
